33ad19dbe5d3df79705e638a9d6b253a102d436b
MrIyut <77267521+MrIyut@users.noreply.github.com>
2024-02-25 23:58:49 +0200

    Update aws.c

--- aws.c
+++ aws.c
@@ -29,8 +29,6 @@ static int listenfd;
 /* epoll file descriptor */
 static int epollfd;
 
-static io_context_t ctx;
-
 static int aws_on_path_cb(http_parser *p, const char *buf, size_t len)
 {
 	struct connection *conn = (struct connection *)p->data;
@@ -45,7 +43,7 @@ static int aws_on_path_cb(http_parser *p, const char *buf, size_t len)
 
 static void connection_prepare_send_404(struct connection *conn)
 {
-	/* TODO: Prepare the connection buffer to send the 404 header. */
+	/* Prepare the connection buffer to send the 404 header. */
 	memcpy(conn->send_buffer, NOT_FOUND, strlen(NOT_FOUND));
 	conn->send_len = strlen(NOT_FOUND);
 	dlog(LOG_INFO, "Failed to open the file at %s\n", conn->request_path);
@@ -53,9 +51,9 @@ static void connection_prepare_send_404(struct connection *conn)
 
 static void connection_prepare_send_reply_header(struct connection *conn)
 {
-	/* TODO: Prepare the connection buffer to send the reply header. */
+	/* Prepare the connection buffer to send the reply header. */
 	struct stat file_stats;
-
+	
 	fstat(conn->fd, &file_stats);
 	conn->file_size = file_stats.st_size;
 	conn->file_pos = 0;
@@ -68,7 +66,7 @@ static void connection_prepare_send_reply_header(struct connection *conn)
 
 static enum resource_type connection_get_resource_type(struct connection *conn)
 {
-	/* TODO: Get resource type depending on request path/filename. Filename should
+	/* Get resource type depending on request path/filename. Filename should
 	 * point to the static or dynamic folder.
 	 */
 	if (strstr(conn->request_path, AWS_REL_STATIC_FOLDER))
@@ -91,11 +89,9 @@ struct connection *connection_create(int sockfd)
 
 void connection_remove(struct connection *conn)
 {
-	/* TODO: Remove connection handler. */
+	/* Remove connection handler. */
 	int rc = w_epoll_remove_ptr(epollfd, conn->sockfd, conn);
-
 	DIE(rc < 0, "failed to remove connection handler");
-
 	if (conn->fd > 0)
 		close(conn->fd);
 	if (conn->eventfd > 0) 
@@ -111,27 +107,27 @@ void connection_remove(struct connection *conn)
 
 void handle_new_connection(void)
 {
-	/* TODO: Handle a new connection request on the server socket. */
+	/* Handle a new connection request on the server socket. */
 	static int sockfd;
 	socklen_t addrlen = sizeof(struct sockaddr_in);
 	struct sockaddr_in addr;
 	int rc;
 
-	/* TODO: Accept new connection. */
+	/* Accept new connection. */
 	sockfd = accept(listenfd, (SSA *)&addr, &addrlen);
 	DIE(sockfd < 0, "failed to accept new connection");
 	dlog(LOG_INFO, "Accepted a new connection made from: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
 
-	/* TODO: Set socket to be non-blocking. */
+	/* Set socket to be non-blocking. */
 	/* Make sure to keep previous flags*/
 	int previous_flags = fcntl(sockfd, F_GETFL, 0);
 	fcntl(sockfd, F_SETFL, previous_flags | O_NONBLOCK);
-	/* TODO: Instantiate new connection handler. */
+	/* Instantiate new connection handler. */
 	struct connection *conn = connection_create(sockfd);
-	/* TODO: Add socket to epoll. */
+	/* Add socket to epoll. */
 	rc = w_epoll_add_ptr_in(epollfd, sockfd, conn);
 	DIE(rc < 0, "failed to add socket to epoll");
-	/* TODO: Initialize HTTP_REQUEST parser. */
+	/* Initialize HTTP_REQUEST parser. */
 	dlog(LOG_INFO, "Successfully initialized socket connection %d, %p\n", sockfd, conn);
 	http_parser_init(&conn->request_parser, HTTP_REQUEST);
 	conn->request_parser.data = conn;
@@ -139,24 +135,22 @@ void handle_new_connection(void)
 
 int connection_open_file(struct connection *conn)
 {
-	/* TODO: Open file and update connection fields. */
+	/* Open file and update connection fields. */
 	conn->fd = open(conn->request_path, O_RDWR);
 	if (conn->fd == -1)
 		connection_prepare_send_404(conn);
 	else
 		connection_prepare_send_reply_header(conn);
 
-	/*allow for out events on request file*/
+	/* Allow for out events on request file. */
 	int rc = w_epoll_update_ptr_inout(epollfd, conn->sockfd, conn);
-
 	DIE(rc < 0, "failed to update connection handler");
 	return conn->fd == -1 ? STATE_SENDING_404 : STATE_SENDING_HEADER;
 }
 
 int parse_header(struct connection *conn)
 {
-	/* TODO: Parse the HTTP header and extract the file path. */
-	/* Use mostly null settings except for on_path callback. */
+	/* Parse the HTTP header and extract the file path. */
 	http_parser_settings settings_on_path = {
 		.on_message_begin = 0,
 		.on_header_field = 0,
@@ -170,7 +164,6 @@ int parse_header(struct connection *conn)
 		.on_message_complete = 0};
 
 	int bytes_parsed = http_parser_execute(&conn->request_parser, &settings_on_path, conn->recv_buffer, conn->recv_len);
-
 	dlog(LOG_INFO, "Parsed HTTP request path (%d bytes): %s\n", bytes_parsed, conn->request_path);
 	conn->res_type = connection_get_resource_type(conn);
 	conn->state = connection_open_file(conn);
@@ -179,9 +172,7 @@ int parse_header(struct connection *conn)
 
 void receive_data(struct connection *conn)
 {
-	/* TODO: Receive message on socket.
-	 * Store message in recv_buffer in struct connection.
-	 */
+	/* Receive message on socket. */
 	char addressBuffer[64];
 	get_peer_address(conn->sockfd, addressBuffer, 64);
 	size_t len = BUFSIZ;
@@ -189,8 +180,7 @@ void receive_data(struct connection *conn)
 
 	if (recv_len <= 0) {
 		/* error in communication ( <0 ) or connection closed (0)*/
-		dlog(LOG_INFO, "%s, peer: %s\n", recv_len == 0 ?
-		"Receiving connection closed" : "Receiving communication error", addressBuffer);
+		dlog(LOG_INFO, "%s, peer: %s\n", recv_len == 0 ? "Receiving connection closed" : "Receiving communication error", addressBuffer);
 		conn->state = STATE_CONNECTION_CLOSED;
 		return;
 	}
@@ -204,28 +194,24 @@ void receive_data(struct connection *conn)
 ssize_t connection_send_data(struct connection *conn)
 {
 	/* May be used as a helper function. */
-	/* TODO: Send as much data as possible from the connection send buffer.
+	/* Send as much data as possible from the connection send buffer.
 	 * Returns the number of bytes sent or -1 if an error occurred
 	 */
 	char addressBuffer[64];
-
 	get_peer_address(conn->sockfd, addressBuffer, 64);
-
 	if (conn->send_len < 0) {
 		dlog(LOG_INFO, "No data to send to %s\n", addressBuffer);
 		return -1;
 	}
 
 	ssize_t total_sent = 0;
-
 	while (conn->send_len > 0) {
 		size_t len = conn->send_len;
 		ssize_t send_len = send(conn->sockfd, conn->send_buffer, len, MSG_DONTWAIT); // conn->send_len => len
 
 		if (send_len <= 0) {
 			/* communication error ( <0 ) or connection was closed (0)*/
-			dlog(LOG_INFO, "%s, peer: %s\n", send_len == 0 ?
-			"Connection was closed while sending data" : "Communication error while sending data", addressBuffer);
+			dlog(LOG_INFO, "%s, peer: %s\n", send_len == 0 ? "Connection was closed while sending data" : "Communication error while sending data", addressBuffer);
 			conn->state = STATE_CONNECTION_CLOSED;
 			return -1;
 		}
@@ -246,7 +232,7 @@ ssize_t connection_send_data(struct connection *conn)
 
 enum connection_state connection_send_static(struct connection *conn)
 {
-	/* TODO: Send static data using sendfile(2). */
+	/* Send static data. */
 	while (conn->file_size > 0) {
 		size_t count = conn->file_size;
 		size_t send_len = sendfile(conn->sockfd, conn->fd, NULL, count); // conn->file_size => count
@@ -272,9 +258,7 @@ enum connection_state connection_send_static(struct connection *conn)
 
 void connection_start_async_io(struct connection *conn)
 {
-	/* TODO: Start asynchronous operation (read from file).
-	 * Use io_submit(2) & friends for reading data asynchronously.
-	 */
+	/* Start asynchronous operation (read from file). */
 	conn->eventfd = eventfd(0, 0);
 	conn->async_data = calloc(conn->file_size, sizeof(char));
 	io_prep_pread(&conn->iocb, conn->fd, conn->async_data, conn->file_size, 0);
@@ -283,7 +267,6 @@ void connection_start_async_io(struct connection *conn)
 	io_set_eventfd(&conn->iocb, conn->eventfd);
 
 	int rc = w_epoll_add_ptr_in(epollfd, conn->eventfd, conn);
-
 	DIE(rc < 0, "failed to add in notification for when file is ready to be read");
 
 	io_setup(1, &conn->ctx);
@@ -293,11 +276,10 @@ void connection_start_async_io(struct connection *conn)
 
 void connection_complete_async_io(struct connection *conn)
 {
-	/* TODO: Complete asynchronous operation; operation returns successfully.
+	/* Complete asynchronous operation; operation returns successfully.
 	 * Prepare socket for sending.
 	 */
 	int rc = read(conn->eventfd, &conn->async_read_len, sizeof(size_t));
-
 	DIE(rc <= 0, "failed to prepare file for sending");
 
 	rc = w_epoll_update_ptr_inout(epollfd, conn->eventfd, conn);
@@ -309,7 +291,6 @@ void connection_complete_async_io(struct connection *conn)
 void connection_send_dynamic_data(struct connection *conn)
 {
 	char addressBuffer[64];
-
 	get_peer_address(conn->sockfd, addressBuffer, 64);
 
 	if (conn->file_size == 0) {
@@ -319,16 +300,10 @@ void connection_send_dynamic_data(struct connection *conn)
 	}
 
 	size_t len = conn->file_size;
-
-	if (len > 1)
-		len /= 2;
-
 	ssize_t send_len = send(conn->sockfd, conn->async_data, len, MSG_DONTWAIT); // conn->file_size => len
-
 	if (send_len <= 0) {
 		/* error in communication ( <0 ) or connection closed (0)*/
-		dlog(LOG_INFO, "%s, peer: %s\n", send_len == 0 ?
-		"Dynamic send connection closed" : "Dynamic send communication error", addressBuffer);
+		dlog(LOG_INFO, "%s, peer: %s\n", send_len == 0 ? "Dynamic send connection closed" : "Dynamic send communication error", addressBuffer);
 		return;
 	}
 
@@ -336,21 +311,19 @@ void connection_send_dynamic_data(struct connection *conn)
 	memcpy(conn->async_data, conn->async_data + send_len, conn->file_size);
 	conn->async_data[conn->file_size] = '\0';
 
-	dlog(LOG_INFO, "Dynamically sent %lu bytes from file %s to %s, remaining data (%lu bytes)\n",
-	 send_len, conn->request_path, addressBuffer, conn->file_size);
+	dlog(LOG_INFO, "Dynamically sent %lu bytes from file %s to %s, remaining data (%lu bytes)\n", send_len, conn->request_path, addressBuffer, conn->file_size);
 }
 
 void connection_send_dynamic(struct connection *conn)
 {
-	/* TODO: Read data asynchronously.
+	/* Read data asynchronously.
 	 * Returns 0 on success and -1 on error.
 	 */
 	if (conn->eventfd == -1)
 		connection_start_async_io(conn);
 
-	/*there will be no more out events*/
+	/* there will be no more out events. */
 	int rc = w_epoll_update_ptr_in(epollfd, conn->sockfd, conn);
-
 	DIE(rc < 0, "failed to update connection handler");
 }
 
@@ -359,9 +332,7 @@ void connection_send_file(struct connection *conn)
 	if (conn->fd == -1) {
 		/*sent all data, wait for more requests*/
 		int rc = w_epoll_update_ptr_in(epollfd, conn->sockfd, conn);
-
 		DIE(rc < 0, "failed to update connection handler");
-
 		conn->state = STATE_INITIAL;
 		return;
 	}
@@ -373,7 +344,6 @@ void connection_send_file(struct connection *conn)
 
 	if (conn->res_type == RESOURCE_TYPE_DYNAMIC) {
 		connection_send_dynamic(conn);
-
 		return;
 	}
 	conn->state = connection_send_static(conn);
@@ -381,7 +351,7 @@ void connection_send_file(struct connection *conn)
 
 void handle_input(struct connection *conn)
 {
-	/* TODO: Handle input information: may be a new message or notification of
+	/* Handle input information: may be a new message or notification of
 	 * completion of an asynchronous I/O operation.
 	 */
 	switch (conn->state) {
@@ -402,7 +372,7 @@ void handle_input(struct connection *conn)
 
 void handle_output(struct connection *conn)
 {
-	/* TODO: Handle output information: may be a new valid requests or notification of
+	/* Handle output information: may be a new valid requests or notification of
 	 * completion of an asynchronous I/O operation or invalid requests.
 	 */
 	switch (conn->state) {
@@ -429,7 +399,7 @@ void handle_output(struct connection *conn)
 
 void handle_client(uint32_t event, struct connection *conn)
 {
-	/* TODO: Handle new client. There can be input and output connections.
+	/* Handle new client. There can be input and output connections.
 	 * Take care of what happened at the end of a connection.
 	 */
 	if (event & EPOLLOUT)
@@ -440,18 +410,15 @@ void handle_client(uint32_t event, struct connection *conn)
 
 int main(void)
 {
-	/* TODO: Initialize asynchronous operations. */
-	io_setup(10, &ctx);
-
-	/* TODO: Initialize multiplexing. */
+	/* Initialize multiplexing. */
 	epollfd = w_epoll_create();
 	DIE(epollfd < 0, "failed to init multiplexing");
 
-	/* TODO: Create server socket. */
+	/* Create server socket. */
 	listenfd = tcp_create_listener(AWS_LISTEN_PORT, DEFAULT_LISTEN_BACKLOG);
 	DIE(listenfd < 0, "failed to create server socker");
 
-	/* TODO: Add server socket to epoll object*/
+	/* Add server socket to epoll object*/
 	int rc = w_epoll_add_fd_in(epollfd, listenfd);
 	DIE(rc < 0, "failed to add server socker to epoll object");
 
@@ -461,12 +428,11 @@ int main(void)
 	/* server main loop */
 	while (1) {
 		struct epoll_event rev;
-
-		/* TODO: Wait for events. */
+		/* Wait for events. */
 		int rc_wait = w_epoll_wait_infinite(epollfd, &rev);
 		DIE(rc_wait < 0, "server failed to wait");
 
-		/* TODO: Switch event types; consider
+		/* Switch event types; consider
 		 *   - new connection requests (on server socket)
 		 *   - socket communication (on connection sockets)
 		 */
@@ -478,13 +444,11 @@ int main(void)
 		}
 
 		struct connection *conn = (struct connection *) rev.data.ptr;
-
 		dlog(LOG_INFO, "Handling client event: %d, on fd: %d, is input event: %d, is output event: %d, state: %d\n",
 		 rev.events, rev.data.fd, rev.events & EPOLLIN, rev.events & EPOLLOUT, conn->state);
 		handle_client(rev.events, rev.data.ptr);
 	}
 
 	close(epollfd);
-	io_destroy(ctx);
 	return 0;
 }
